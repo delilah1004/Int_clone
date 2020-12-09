f253580e06c3cbef572b4c35123c9f7ebec9a5fd
delilah1004 <pskdy8414@naver.com>
2020-12-09 17:57:07 +0900

    wave start

---
+++ wave/app.js
@@ -0,0 +1,12 @@
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        document.body.appendChild(this.canvas);
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        requestAnimationFrame(this.animate.bind(this));
+    }
+}
